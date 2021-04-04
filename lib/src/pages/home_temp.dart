17b0a138deb2571c94068d932126bd2c34e62b5b
luisalbertmschz <luisalbertmsanchez@gmail.com>
2021-04-04 02:19:57 -0400

    [ ☄ ] Probando ListView, ListTile y Divider de forma no dinámica.

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -5,11 +5,47 @@ import 'package:flutter/material.dart';
 
 
  class HomePageTemp extends StatelessWidget {
+
+
+   
  
    @override
    Widget build(BuildContext context) {
-     return Center(
-       child: Text('Home Temp Componentes'),
-     );
+     return Scaffold(
+
+         appBar:  AppBar(
+
+           title: Text('Componentes'),
+
+         ),
+
+         body: ListView(
+
+           children: [
+
+             ListTile(
+              title: Text('Componente 1 Lista'),
+             ),
+
+             Divider(),
+
+                          ListTile(
+              title: Text('Componente 2 Lista'),
+             ),
+
+             
+             Divider(),
+
+                          ListTile(
+              title: Text('Componente 3 Lista'),
+             ),
+
+
+           ],
+         ),
+
+       );
    }
+
+
  }
